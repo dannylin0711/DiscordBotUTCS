7eeda697d249cd9b67950cd6dcb985853bb4bbe7
dannylin0711 <dannylin0711@gmail.com>
2019-09-26 11:40:28 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,5 +14,5 @@ async def on_ready():
 async def add(ctx, a: int, b: int):
     await ctx.send(a+b)
 
-bot.login("NjI2NjA1NjQxMDkxNTE0Mzc4.XYwxlg.Q-6QtW7Q9Ipq9AJ5-iwWnKtaS9o")
-bot.run('NjI2NjA1NjQxMDkxNTE0Mzc4.XYwxlg.Q-6QtW7Q9Ipq9AJ5-iwWnKtaS9o')
+
+bot.run('NjI2NjA1NjQxMDkxNTE0Mzc4.XYwzFg.bKob86Qwh4HkNnCFbutEStOj7VU')
