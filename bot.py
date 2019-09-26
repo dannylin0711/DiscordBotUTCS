58eba1d843eb9d87b843af192e6fa70154a40da6
dannylin0711 <dannylin0711@gmail.com>
2019-09-26 11:49:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,5 +14,9 @@ async def on_ready():
 async def add(ctx, a: int, b: int):
     await ctx.send(a+b)
 
+@bot.command()
+async def laugh(ctx):
+    await ctx.send('哈哈...'))
+
 
 bot.run('NjI2NjA1NjQxMDkxNTE0Mzc4.XYwzFg.bKob86Qwh4HkNnCFbutEStOj7VU')
