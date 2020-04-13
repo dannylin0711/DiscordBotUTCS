0d94c0be7386e06778aaf03b50b01a1556633ea8
dannylin0711 <dannylin0711@gmail.com>
2020-04-13 13:53:07 +0800

    Revert "Big update"
    This reverts commit 2ed1311f06456884a5b0e66dff9d3581f42a027d.

--- bot.py
+++ bot.py
@@ -11,22 +11,88 @@ client = Client
 guild = Guild
 bot = commands.Bot(command_prefix="$", description='A bot that greets the user back.')
 startuptime = datetime.datetime.now()
-#discord.opus.load_opus('libopus.dylib')
 
-
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    activity = discord.Activity(name='RRRR', type=discord.ActivityType.playing, state='我在玩啦', details='時間是我已啟動的時間' , start=startuptime)
+    await bot.change_presence(activity=activity)
+    
+@bot.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == bot.user:
+        return
+    if 'RRRR' in message.content:
+        emoji = bot.get_emoji(623835905999896626)
+        await message.add_reaction(emoji)
+    await bot.process_commands(message)
 
 @bot.command()
-async def load(ctx, extension):
-    bot.load_extension(f'cogs.{extension}')
-    
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
 @bot.command()
-async def unload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
+async def laugh(ctx):
+    await ctx.send('哈哈...')
+
+@bot.command()
+async def 測試(ctx):
+    await ctx.send('你到底想幹嘛')
+
+@bot.command()
+async def penguinLaugh(ctx):
+
+    await ctx.send("""<:Penguin:623835905999896626>
+<:Penguin:623835905999896626><:Penguin:623835905999896626>
+<:Penguin:623835905999896626><:Penguin:623835905999896626><:Penguin:623835905999896626>
+<:Penguin:623835905999896626><:Penguin:623835905999896626><:Penguin:623835905999896626><:Penguin:623835905999896626>
+<:Penguin:623835905999896626><:Penguin:623835905999896626><:Penguin:623835905999896626>
+<:Penguin:623835905999896626><:Penguin:623835905999896626>
+<:Penguin:623835905999896626>""")
+
+@bot.command()
+async def emoji(ctx):
+    emojiList = bot.emojis
+    emojiString = "<:"
+    emojiString += (emojiList[1].name + ":"+str(emojiList[1].id))+">"
+    await ctx.send(emojiString)
     
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
 
+@bot.command()
+async def nameOfGuild(ctx):
+    
+    await ctx.send('name')
 
-BOT_TOKEN = 'NjI2NjA1NjQxMDkxNTE0Mzc4.XbP6Qg.oAQ93md_USdkF3dDNNDbx-A0RCE'
+@bot.command()
+async def test(ctx):
+    await ctx.send("""This is a test message!
+這是測試訊息""")
+
+@bot.command()
+async def hahaha(ctx):
+    await ctx.send("""HAHAHAHAHAHAHAHAHAHA""")
+
+@bot.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.voice_client
+    await server.disconnect()
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send('打錯指令辣')
+        return
+    raise error
+
+BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+
 bot.run(BOT_TOKEN)
