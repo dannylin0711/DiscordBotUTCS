cd7b5a57707ac47bfcfae0395c6094d091ad75b0
dannylin0711 <dannylin0711@gmail.com>
2020-09-24 14:20:51 +0800

    更新機器人刪除編輯訊息不推送Log

--- cogs/event.py
+++ cogs/event.py
@@ -43,6 +43,28 @@ class Events(commands.Cog):
             return
         raise error
     
+    @commands.Cog.listener()
+    async def on_message_delete(self,message):
+        if not message.author.bot:
+            print("\n已刪除的訊息:")
+            print("　伺服器　　:"+ message.channel.guild.name)
+            print("　頻道　　　:"+ message.channel.name)
+            print("　訊息發出者:"+ message.author.display_name)
+            print("　時間　　　:"+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            print("　訊息內容　:"+message.content+"\n")
+        
+    @commands.Cog.listener()
+    async def on_message_edit(self,before, after):
+        if not before.author.bot:
+            print("\n已修改的訊息:")
+            print("　伺服器　　:"+ before.channel.guild.name)
+            print("　頻道　　　:"+ before.channel.name)
+            print("　訊息發出者:"+ before.author.display_name)
+            print("　時間　　　:"+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            print("　修改前內容:"+ before.content)
+            print("　修改後內容:"+ after.content+"\n")
+        
+    
     def __init__(self,bot):
         self.bot = bot
         
